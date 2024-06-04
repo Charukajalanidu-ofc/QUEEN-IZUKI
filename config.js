69230a933fb47f865ee158a1ba9089cf0015b943
DarkMrYasiya <161712092+DarkMrYasiya@users.noreply.github.com>
2024-06-04 02:27:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,19 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
-POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
-LANG: process.env.BOT_LANG || 'EN' ,
-PREFIX:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX,
-ANTI_BAD: process.env.ANTI_BAD === undefined ? false : process.env.ANTI_BAD ,
-MAX_SIZE: process.env.MAX_SIZE || 300,
-WORK_TYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE ,
-ANTI_LINK: process.env.ANTI_LINK === undefined ? false : process.env.ANTI_LINK ,
-ANTI_BOT: process.env.ANTI_BOT === undefined ? false : process.env.ANTI_BOT ,
-ALIVE: process.env.ALIVE || `default`,
-AUTO_VOICE:  process.env.AUTO_VOICE  === undefined ? false : process.env.AUTO_VOICE ,
-AUTO_STICKER: process.env.AUTO_STICKER === undefined ? false : process.env.AUTO_STICKER ,
-NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK === undefined ? false : process.env.NUMBER_212_BLOCK ,
-FOOTER: process.env.FOOTER ||  '©ＱＵＥＥＮ -ＩＺＵＭＩ - ＭＤ',
-LOGO: process.env.LOGO || `https://telegra.ph/file/ba8ea739e63bf28c30b37.jpg` 
+SESSION_ID : process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID ,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? "" : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? "" : process.env.GITHUB_USERNAME,
+BOT_NUMBER: process.env.BOT_NUMBER === undefined ? "" : process.env.BOT_NUMBER,
 };
